a09b1f181b232b4ebd1421c735300cc9808695cd
Yahub <johndoe@example.com>
2019-08-12 13:36:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotabuff/precache-manifest.e4c2220d2286b3002954f70db4fe6924.js"
+  "/dotabuff/precache-manifest.285e571fcae0d51cdd588c30db61a911.js"
 );
 
 self.addEventListener('message', (event) => {
