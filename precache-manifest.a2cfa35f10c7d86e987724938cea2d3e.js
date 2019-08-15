dd1ff1197bdbe747d736f257b7715a60932d7fe3
Yahub <johndoe@example.com>
2019-08-15 17:19:28 +0300

    Updates

---
+++ precache-manifest.a2cfa35f10c7d86e987724938cea2d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d084dd31d77995aea709210fcce7f33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c6aa156889b23bcb3c",
+    "url": "/static/css/main.340823eb.chunk.css"
+  },
+  {
+    "revision": "1f693cfc46b6da6963cb",
+    "url": "/static/js/2.1aecf452.chunk.js"
+  },
+  {
+    "revision": "31c6aa156889b23bcb3c",
+    "url": "/static/js/main.198372c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotabuff/precache-manifest.8321e029d6d75f1aa346a4b94024dc9f.js"
+  "/precache-manifest.a2cfa35f10c7d86e987724938cea2d3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dotabuff/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
