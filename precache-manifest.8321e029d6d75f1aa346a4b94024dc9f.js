9162017a4ebe173f52102bfe723a08a5a3acfabf
Yahub <johndoe@example.com>
2019-08-15 17:44:54 +0300

    Updates

---
+++ precache-manifest.8321e029d6d75f1aa346a4b94024dc9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bc745becc0b9114468d12f135a10d1",
+    "url": "/dotabuff/index.html"
+  },
+  {
+    "revision": "e878b24cf3f6237bdd51",
+    "url": "/dotabuff/static/css/main.340823eb.chunk.css"
+  },
+  {
+    "revision": "1f693cfc46b6da6963cb",
+    "url": "/dotabuff/static/js/2.1aecf452.chunk.js"
+  },
+  {
+    "revision": "e878b24cf3f6237bdd51",
+    "url": "/dotabuff/static/js/main.18672152.chunk.js"
+  },
+  {
+    "revision": "d6dc326819622f5241df",
+    "url": "/dotabuff/static/js/runtime~main.fa49d422.js"
+  }
+]);
