22f2381d5f7f83b9f322bec2abd3a45f9a143ac1
Yahub <johndoe@example.com>
2019-08-21 14:47:49 +0300

    Updates

---
+++ precache-manifest.2810dcd67d6cb07445c56e73718340ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d782ba9f8e12929379ca613be240ef",
+    "url": "/db2/index.html"
+  },
+  {
+    "revision": "331e05ce9165945dae4a",
+    "url": "/db2/static/css/main.e645ac58.chunk.css"
+  },
+  {
+    "revision": "a0dc21251149c572751f",
+    "url": "/db2/static/js/2.0b524471.chunk.js"
+  },
+  {
+    "revision": "331e05ce9165945dae4a",
+    "url": "/db2/static/js/main.8d473d6e.chunk.js"
+  },
+  {
+    "revision": "56b286e5b8b6868e33af",
+    "url": "/db2/static/js/runtime~main.461fd023.js"
+  }
+]);
